403bed76dcfa6045a6ea9a0cda697c042ab42c6d
FireballGIT <gafutb@outlook.com>
2026-01-13 16:46:43 -0600

    Remove unused code from help.js
    Removed unused imports and the help command implementation.

--- commands/general/help.js
+++ commands/general/help.js
@@ -25,30 +25,4 @@ module.exports = {
     message.channel.send(reply);
   }
 };
-const fs = require("fs");
-const path = require("path");
-const { getPermissionLevel } = require("../../utils/permissionCheck");
-const permissions = require("../../config/permissions.json");
 
-module.exports = {
-  name: "help",
-  requiredRole: "Member",
-  execute(message) {
-    const prefix = "?";
-    const memberLevel = getPermissionLevel(message.member);
-
-    const commandsDir = path.join(__dirname);
-    const files = fs.readdirSync(commandsDir).filter(f => f.endsWith(".js"));
-
-    let reply = "**Firebot Commands:**\n";
-
-    files.forEach(file => {
-      const cmd = require(path.join(commandsDir, file));
-      const cmdLevel = permissions[cmd.requiredRole] ?? 0;
-      const color = memberLevel >= cmdLevel ? "🟢" : "🔴";
-      reply += `${color} \`${prefix}${cmd.name}\`\n`;
-    });
-
-    message.channel.send(reply);
-  }
-};
